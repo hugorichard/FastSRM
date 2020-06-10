a22406b92b4ddd9ba3eb2c4a444f1d725f34c595
hrichard <hugo.richard@inria.fr>
2020-06-10 12:16:23 +0200

    working version

--- README.md
+++ README.md
@@ -6,22 +6,7 @@ See https://arxiv.org/pdf/1909.12537.pdf
 Install
 ---------
 
-Move into FastSRM directory
-
-``cd FastSRM``
-
-Create virual environment
-
-`virtualenv -p python3 venv`
-
-
-Activate virtual environment
-
-`source venv/bin/activate`
-
-Install FastSRM
-
-`pip install -e .`
+`pip install fastsrm`
 
 Docs
 ------
--- setup.py
+++ setup.py
@@ -5,17 +5,24 @@ import sys
 import setuptools
 from copy import deepcopy
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='fastsrm',
     install_requires=[
         'scikit-learn',
-        'joblib'
+        'joblib',
+        'picard'
     ],
+    version = '0.0.2',
+    license='MIT',
     author='Hugo RICHARD',
     author_email='hugo.richard@inria.fr',
     url='https://github.com/hugorichard/FastSRM',
+    long_description=long_description,
     description='Fast Shared response model',
-    keywords='neuroscience, algorithm, fMRI, distributed, scalable',
+    keywords='Component analysis, fMRI',
     packages=find_packages(),
     python_requires='>=3',
 )
