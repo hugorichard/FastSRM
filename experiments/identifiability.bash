56acab9652e2fde7770453fdcb822a14c17eaac2
hrichard <hugo.richard@inria.fr>
2021-11-04 11:12:58 +0100

    update identifiability

--- experiments/identifiability.bash
+++ experiments/identifiability.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+a = 'identifiability'
 for i in 'brainiak' 'fast'
 do
     export i;
-    sbatch identifiability.sbatch --output=logs/identifiability_$i.out ;
+    sbatch --output=$a.$i.out --export=i=$i identifiability.sbatch
 done
