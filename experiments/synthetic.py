08ae793ea41fc3aa5e676f5214ef80567a61fc69
hrichard <hugo.richard@inria.fr>
2021-11-04 17:47:35 +0100

    update add results

--- experiments/synthetic.py
+++ experiments/synthetic.py
@@ -56,6 +56,7 @@ def do_expe(it, seed, algo):
 
 seeds = np.arange(30)
 iters = 100
+os.makedirs("./results", exist_ok=True)
 for algo in ["detsrm", "probsrm", "fastsrm"]:
     res = Parallel(n_jobs=3, verbose=10)(
         delayed(do_expe)(iters, seed, algo) for seed in seeds
