33f7e4436674c68b7a7514c5b0ad11f8db059c7f
hrichard <hugo.richard@inria.fr>
2021-11-04 11:06:01 +0100

    upda

--- experiments/identifiability.bash
+++ experiments/identifiability.bash
@@ -3,5 +3,5 @@
 for i in 'brainiak' 'fast'
 do
     export i;
-    sbatch identifiability.sbatch --output=logs/identifiability_$i.out
+    sbatch identifiability.sbatch --output=logs/identifiability_$i.out ;
 done
