d701e9504053457e2f599e2d93fde0439d05697f
hrichard <hugo.richard@inria.fr>
2021-11-05 11:37:12 +0100

    add toy example

---
+++ examples/synthetic_toy.py
@@ -0,0 +1,71 @@
+import os
+from time import time
+from fastsrm.fastsrm import fastsrm
+from fastsrm.srm import detsrm, probsrm, projection
+from fastsrm.utils import reg_error
+import numpy as np
+from joblib import delayed, Parallel
+
+
+dim = (50, 50, 50)
+m, v, k, n = 10, np.prod(dim), 50, 1000
+
+
+def do_expe(it, seed, algo):
+    rng = np.random.RandomState(seed)
+    Sigma = rng.dirichlet(np.ones(k), 1).flatten()
+    S = np.sqrt(Sigma)[:, None] * rng.randn(k, n)
+    W = np.array([projection(rng.randn(v, k)) for i in range(m)])
+    sigmas = 0.1 * rng.rand(m)
+    N = np.array([sigmas[i] * rng.randn(v, n) for i in range(m)])
+    X = np.array([W[i].dot(S) + N[i] for i in range(m)])
+    S_true = S
+    t_init = time()
+
+    def callback(source, gnorm, current_iter, current_time):
+        return (
+            float(reg_error(np.copy(S_true), np.copy(source))),
+            float(current_time - t_init),
+            current_iter,
+            seed,
+            algo,
+            float(gnorm),
+        )
+
+    if algo == "detsrm":
+        S = detsrm(
+            X, k, n_iter=it, random_state=rng, callback=callback, tol=-1,
+        )[-1]
+    if algo == "probsrm":
+        S = probsrm(
+            X, k, n_iter=it, random_state=rng, callback=callback, tol=-1,
+        )[-1]
+    if algo == "fastdet":
+        S = fastsrm(
+            [[x] for x in X],
+            k,
+            n_iter=it,
+            random_state=rng,
+            callback=callback,
+            tol=-1,
+            method="det",
+        )[-1]
+    if algo == "fastprob":
+        S = fastsrm(
+            [[x] for x in X],
+            k,
+            n_iter=it,
+            random_state=rng,
+            callback=callback,
+            tol=-1,
+            method="prob",
+        )[-1]
+    return np.array(S)
+
+
+iters = 20
+os.makedirs("./results", exist_ok=True)
+for algo in ["detsrm", "probsrm", "fastdet", "fastprob"]:
+    S = do_expe(20, 0, algo)
+
+
