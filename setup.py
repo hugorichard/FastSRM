9697389f431a8cb9995b8aaaa63c1c7c71efd1b4
hrichard <hugo.richard@inria.fr>
2021-11-25 14:04:30 +0100

    update FastSRM

--- setup.py
+++ setup.py
@@ -9,22 +9,24 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='fastsrm',
+    name="fastsrm",
     install_requires=[
-        'scikit-learn',
-        'joblib',
-        'tqdm',
-        'matplotlib'
+        "scipy>=0.18.0"
+        "numpy>=1.12"
+        "scikit-learn>=0.23"
+        "joblib>=1.1.0"
+        "matplotlib>=2.0.0"
+        "pytest>=6.2.5"
     ],
-    version = '0.0.3',
-    license='MIT',
-    author='Hugo RICHARD',
-    download_url='https://github.com/hugorichard/FastSRM/archive/v_002.tar.gz',
-    author_email='hugo.richard@inria.fr',
-    url='https://github.com/hugorichard/FastSRM',
+    version="0.0.3",
+    license="MIT",
+    author="Hugo RICHARD",
+    download_url="https://github.com/hugorichard/FastSRM/archive/v_002.tar.gz",
+    author_email="hugo.richard@inria.fr",
+    url="https://github.com/hugorichard/FastSRM",
     long_description=long_description,
-    description='Fast Shared response model',
-    keywords='Component analysis, fMRI',
+    description="Fast Shared response model",
+    keywords="Component analysis, fMRI",
     packages=find_packages(),
-    python_requires='>=3',
+    python_requires=">=3",
 )
